04938a40339681710f0a340f69f38aac92a55388
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-03 11:22:54 +0700

    membuat route post untuk menambah data music

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const db = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: false 
+})
+);
+
+app.listen(PORT, () => {
+    console.log("Server started on port 3000");
+});
+
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log("Server started");
+        })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+app.post("/music", async(req, res)=>{
+    const data = req.body;
+    try{
+        const music = await db.Music.create(data);
+        res.send(music);
+    } catch(err){
+        res.send(err);
+    }
+})
+
+
